582f0149f006e148f1fb611ad4ef37871e693dbf
Gorka <gorkacarmonapino@gmail.com>
2025-02-26 21:26:26 +0100

    Produccion

--- luxury-villas-app/gulpfile.js
+++ luxury-villas-app/gulpfile.js
@@ -13,6 +13,18 @@ const paths = {
   scripts: {
     src: 'src/js/**/*.js',
     dest: 'dist/js'
+  },
+  html: {
+    src: 'src/index.html', // Ruta al archivo index.html
+    dest: 'dist' // Carpeta destino
+  },
+  assets: {
+    src: 'src/assets/**/*', // Ruta a todos los archivos en assets
+    dest: 'dist/assets' // Carpeta destino para assets
+  },
+  css: {
+    src: 'src/assets/css/**/*.css',
+    dest: 'dist/css'
   }
 };
 
@@ -25,6 +37,12 @@ function styles() {
     .pipe(gulp.dest(paths.styles.dest));
 }
 
+// Tarea para copiar archivos CSS
+function copyCss() {
+  return gulp.src(paths.css.src)
+    .pipe(gulp.dest(paths.css.dest));
+}
+
 // Tarea para minificar y concatenar JavaScript
 function scripts() {
   return gulp.src(paths.scripts.src)
@@ -35,17 +53,38 @@ function scripts() {
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
+// Tarea para copiar el archivo index.html
+function copyHtml() {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest));
+}
+
+// Tarea para copiar los archivos de assets
+function copyAssets() {
+  return gulp.src(paths.assets.src)
+    .pipe(gulp.dest(paths.assets.dest));
+}
+
 // Tarea para observar cambios en los archivos
 function watch() {
   gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.css.src, copyCss);
   gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.html.src, copyHtml);
+  gulp.watch(paths.assets.src, copyAssets);
 }
 
 // Tarea por defecto
-const build = gulp.series(gulp.parallel(styles, scripts), watch);
+const build = gulp.series(
+  gulp.parallel(styles, copyCss, scripts, copyHtml, copyAssets), // Ejecutar styles, copyCss, scripts, copyHtml y copyAssets
+  watch
+);
 
 exports.styles = styles;
+exports.copyCss = copyCss;
 exports.scripts = scripts;
+exports.copyHtml = copyHtml;
+exports.copyAssets = copyAssets;
 exports.watch = watch;
 exports.build = build;
 exports.default = build;
